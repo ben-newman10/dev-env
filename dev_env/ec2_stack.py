5d34c7a0e1e1470c93956afb9e42827642ef2d42
Ben N <ben.n@dev.net>
2023-06-02 12:49:53 +0100

    abstract network setup into its own construct

--- dev_env/ec2_stack.py
+++ dev_env/ec2_stack.py
@@ -6,53 +6,23 @@ from aws_cdk import (
 from constructs import Construct
 
 from dev_env.autoshutdown_lambda import AutoShutdownLambda
+from dev_env.network_setup import NetworkSetup
 
 
 class Ec2Stack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create Private Subnet
-        private_subnet = ec2.SubnetConfiguration(
-            name="Private",
-            subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
-            cidr_mask=24,
-        )
-
-        # Create Public Subnet
-        public_subnet = ec2.SubnetConfiguration(
-            name="Public",
-            subnet_type=ec2.SubnetType.PUBLIC,
-            cidr_mask=24,
-        )
-
-        # Create VPC and add Subnets
-        vpc = ec2.Vpc(
-            self,
-            "DevVPC",
-            max_azs=2,
-            subnet_configuration=[private_subnet, public_subnet],
-        )
-
-        # Create an Elastic IP for the Nat Gateway
-        eip = ec2.CfnEIP(self, "DevEIP")
-        allocation_id = eip.attr_allocation_id
-
-        # Create NAT Gateway
-        nat_gateway = ec2.CfnNatGateway(
-            self,
-            "DevNatGateway",
-            subnet_id=vpc.public_subnets[0].subnet_id,
-            allocation_id=allocation_id,
-        )
+        # Setup Private network with NAT gateway
+        network = NetworkSetup(self, "NetworkSetup")
 
         # Create EC2 Instance
         instance = ec2.Instance(
             self,
-            "MyInstance",
+            "BenDevInstance",
             instance_type=ec2.InstanceType("t3.micro"),
             machine_image=ec2.AmazonLinuxImage(),
-            vpc=vpc,
+            vpc=network.vpc,
             vpc_subnets=ec2.SubnetSelection(
                 subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
             ),
---
+++ dev_env/network_setup.py
@@ -0,0 +1,45 @@
+from constructs import Construct
+from aws_cdk import aws_ec2 as ec2
+
+
+class NetworkSetup(Construct):
+    @property
+    def vpc(self):
+        return self._vpc
+
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        # Create Private Subnet
+        private_subnet = ec2.SubnetConfiguration(
+            name="Private",
+            subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+            cidr_mask=24,
+        )
+
+        # Create Public Subnet
+        public_subnet = ec2.SubnetConfiguration(
+            name="Public",
+            subnet_type=ec2.SubnetType.PUBLIC,
+            cidr_mask=24,
+        )
+
+        # Create VPC and add Subnets
+        self._vpc = ec2.Vpc(
+            self,
+            "DevVPC",
+            max_azs=2,
+            subnet_configuration=[private_subnet, public_subnet],
+        )
+
+        # Create an Elastic IP for the Nat Gateway
+        eip = ec2.CfnEIP(self, "DevEIP")
+        allocation_id = eip.attr_allocation_id
+
+        # Create NAT Gateway
+        nat_gateway = ec2.CfnNatGateway(
+            self,
+            "DevNatGateway",
+            subnet_id=self._vpc.public_subnets[0].subnet_id,
+            allocation_id=allocation_id,
+        )
